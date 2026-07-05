a345da5fcd57e3a08d9cd8645a84a2994a27dba4
Rithvicca Manimaran <nabitullc@gmail.com>
2026-07-05 11:19:07 -0500

    remove headings and reading themes, show only book covers

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import SearchBar from '../components/ui/SearchBar';
 import Hero from '../components/ui/Hero';
 import TrendingStories from '../components/ui/TrendingStories';
-import ReadingThemes from '../components/ui/ReadingThemes';
 
 export default function Home() {
   return (
@@ -9,7 +8,6 @@ export default function Home() {
       <SearchBar />
       <Hero />
       <TrendingStories />
-      <ReadingThemes />
     </main>
   );
 }
